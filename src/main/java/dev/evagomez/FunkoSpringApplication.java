6c565483c9b6e4691c7ecb091fc3301e31b8bc57
evagmezz <evag.251103@gmail.com>
2023-10-19 20:28:49 +0200

    Initial commit

---
+++ src/main/java/dev/evagomez/FunkoSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.evagomez.funkospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunkoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunkoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/evagomez/funkospring/FunkoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evagomez.funkospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunkoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
