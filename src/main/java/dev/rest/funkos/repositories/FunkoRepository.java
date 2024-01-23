c2a23d4ee6b2c5fa06b5ac17f3be9aedd845fe8f
evagmezz <evag.251103@gmail.com>
2024-01-23 16:13:55 +0100

    cambios

--- src/main/java/dev/rest/funkos/repositories/FunkoRepository.java
+++ src/main/java/dev/rest/funkos/repositories/FunkoRepository.java
@@ -3,6 +3,8 @@ package dev.rest.funkos.repositories;
 import dev.rest.funkos.models.Funko;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +17,5 @@ public interface FunkoRepository extends JpaRepository<Funko, Long>, JpaSpecific
     List<Funko> getByNombreContainingIgnoreCase(String nombre);
 
     List<Funko> getByNombreAndCategoriaNameContainingIgnoreCase(String nombre, String categoria);
+
 }
